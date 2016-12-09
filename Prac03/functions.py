ada25b1246943e6bd03f8b1c9316e725674ff455
jc449799 <srikaustubh.mandaleeka@my.jcu.edu.au>
2016-12-09 20:58:00 +1000

    all workshop programs updated functions

--- Prac03/functions.py
+++ Prac03/functions.py
@@ -1,8 +1,9 @@
-__author__ = 'Cue'
+__author__ = 'jc449799'
+""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
 def square_number(num):
     return(num * num)
 print(square_number(5))
-
+""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
 def is_prime(value):
     i = 2
     while i < value:
@@ -13,15 +14,29 @@ def is_prime(value):
     return True
 
 print(is_prime(5))
-
+""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
 def celsius_to_fahrenheit(celsius):
     return celsius * 1.8 + 32.0
 f = celsius_to_fahrenheit(25)
 print("{}".format(f))
-
+""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
 def inch_to_meter(inches):
     meter = inches/39.37
     return meter
 inch = float(input("Enter number of inches: "))
 meter  = inch_to_meter(inch)
 print("{} inches is {:.4f} meters".format(inch,meter))
+""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+def tax_return(income):
+    tax=0
+    if income >=16000:
+        tax = (income-16000)*0.3
+    return tax
+
+income = float(input("Enter your income: "))
+tax = tax_return(income)
+if tax==0:
+    print("no tax for you")
+else:
+    print("tax for income ${:.2f} is ${:.2f}".format(income,tax))
+""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
